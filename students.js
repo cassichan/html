7447f1737e06bde3281e5dc3bd4ae179e7559335
Cassandra Lee <cassichan@Cassandras-MacBook-Air.local>
2022-08-01 16:57:00 -0400

    fixed spacing

--- students.js
+++ students.js
@@ -7,8 +7,17 @@ const PORT = 4000;
 app.use(cors());
 app.use(express.json());
 
+let students = ["bob", "samantha", "lily", "grace", "jim"];
+
 app.get("/", (req, res) => {
-  res.send("Hello World");
+  let output = "<html><body><ul>"; //start
+  //output = output + "hello there html"; //middle
+  for (let i = 0; i < students.length; i++) {
+    output = output + '<li><input type="checkbox">' + students[i] + "</li>";
+  }
+  output = output + "</ul></body></html>"; //end
+
+  res.send(output);
 });
 
 app.listen(PORT, () => {
